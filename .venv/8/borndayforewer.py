fa3be5e070e8ed6c5ee49b908f6fdfef7324d53f
AlderKonst <koalder@yandex.ru>
2024-05-27 15:26:41 +0300

    sample

---
+++ .venv/8/borndayforewer.py
@@ -0,0 +1,7 @@
+pushkin = ''
+while pushkin != 1799:
+    pushkin = int(input('Какой год рожения Пушкина? '))
+while pushkin != 26.05:
+    pushkin = float(input('Какой день рождения Пушкина (ДД.ММ)? '))
+else:
+    print('Верно!')
---
+++ .venv/8/bornyearforewer.py
@@ -0,0 +1,5 @@
+pushkin = int(input('Какой год рожения Пушкина? '))
+while pushkin != 1799:
+    pushkin = int(input('Неверно!\nКакой год рожения Пушкина? '))
+else:
+    print('Верно')
