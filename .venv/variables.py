90304431004e34015cbb595d23c41961e510ea7e
AlderKonst <koalder@yandex.ru>
2024-05-27 14:43:30 +0300

    sample

--- .venv/variables.py
+++ .venv/variables.py
@@ -2,4 +2,4 @@ insect = 'Пьявица обыкновенная'
 age = 35
 mm = 12.2
 female = False
-print(type(insect), type(age), type(mm), type(female), sep = '/n')
+print(type(insect), type(age), type(mm), type(female), sep = '\n')
